a7c3ec9d51d47e6962110ac1d0c617cbe54c86c9
Вадим <taratonovv8@gmail.com>
2023-04-10 12:25:24 +0300

    Initial commit

---
+++ src/main/java/ru/taratonov/shoppingcart/ShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package ru.taratonov.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(ShoppingCartApplication.class, args);
+	}
+
+}
