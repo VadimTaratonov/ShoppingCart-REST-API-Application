184594851860407c5df204e022c154fcf52247a1
Вадим <taratonovv8@gmail.com>
2023-04-17 16:14:45 +0300

    Added annotation @Repository

--- src/main/java/ru/taratonov/shoppingcart/repository/OrderDetailRepository.java
+++ src/main/java/ru/taratonov/shoppingcart/repository/OrderDetailRepository.java
@@ -1,7 +1,8 @@
 package ru.taratonov.shoppingcart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.taratonov.shoppingcart.model.OrderDetail;
-
+@Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer> {
 }
