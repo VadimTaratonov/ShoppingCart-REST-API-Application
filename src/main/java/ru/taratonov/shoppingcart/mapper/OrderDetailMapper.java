1a0432435e7ca7f51524fb4e6424923bc095bbe5
Вадим <taratonovv8@gmail.com>
2023-04-20 16:28:31 +0300

    Updated Mappers

--- src/main/java/ru/taratonov/shoppingcart/mapper/OrderDetailMapper.java
+++ src/main/java/ru/taratonov/shoppingcart/mapper/OrderDetailMapper.java
@@ -1,16 +1,21 @@
 package ru.taratonov.shoppingcart.mapper;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 import ru.taratonov.shoppingcart.dto.OrderDetailDTO;
 import ru.taratonov.shoppingcart.model.OrderDetail;
 
-@Mapper
+@Mapper(uses = ProductMapper.class)
 public interface OrderDetailMapper {
 
     OrderDetailMapper ORDER_DETAIL_MAPPER = Mappers.getMapper(OrderDetailMapper.class);
 
+    @Mapping(source = "productDTO",target = "product")
     OrderDetail toOrderDetail(OrderDetailDTO orderDetailDTO);
 
+    @InheritInverseConfiguration
     OrderDetailDTO toOrderDetailDTO(OrderDetail orderDetail);
 }
--- src/main/java/ru/taratonov/shoppingcart/mapper/OrderMapper.java
+++ src/main/java/ru/taratonov/shoppingcart/mapper/OrderMapper.java
@@ -1,15 +1,20 @@
 package ru.taratonov.shoppingcart.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import ru.taratonov.shoppingcart.dto.OrderDTO;
 import ru.taratonov.shoppingcart.model.Order;
 
-@Mapper
+@Mapper(uses = { OrderDetailMapper.class, CustomerMapper.class})
 public interface OrderMapper {
     OrderMapper ORDER_MAPPER = Mappers.getMapper(OrderMapper.class);
 
+    @Mapping(source = "customerDTO", target = "customer")
+    @Mapping(source = "orderDetailDTO", target = "orderDetailList")
     Order toOrder(OrderDTO order);
 
+    @Mapping(source = "customer", target = "customerDTO")
+    @Mapping(source = "orderDetailList", target = "orderDetailDTO")
     OrderDTO toOrderDTO(Order order);
 }
